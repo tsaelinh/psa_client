68b078e5f8f52bb3c35bae2a4d9f9de680f45440
Tiffany Saelinh <saelinht@gmail.com>
2021-05-02 18:39:32 -0700

    trying to fix the netlify react router issue

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
+
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render(<Provider store={store}>
                     <App />
